88606450b1a5ce54ae52f5fc3c70587b7f0962e8
MarimbaMaurice <mpanchaud@yahoo.de>
2018-01-28 19:42:38 +0100

    remove clientid from source code

--- app/src/main/java/com/maracuja_juice/spotifynotifications/com/maracuja_juice/spotifynotifications/activities/LoginActivity.java
+++ app/src/main/java/com/maracuja_juice/spotifynotifications/com/maracuja_juice/spotifynotifications/activities/LoginActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.maracuja_juice.spotifynotifications.helper.ConfigReader;
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
 import com.spotify.sdk.android.authentication.AuthenticationResponse;
@@ -15,16 +16,16 @@ public class LoginActivity extends AppCompatActivity {
 
     // Request code will be used to verify if result comes from the login activity. Can be set to any integer.
     private static final int REQUEST_CODE = 1369;
-    private static final String REDIRECT_URI = "spotify-notifications-android://callback";
-    public static final String CLIENT_ID = "2aefe9b8e0c74dffa4da813e218962f3";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_login);
+
+        String clientId = ConfigReader.getConfigValue(this, "clientId");
+        String redirectUri = ConfigReader.getConfigValue(this, "redirectUri");
 
         AuthenticationRequest.Builder builder =
-                new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+                new AuthenticationRequest.Builder(clientId, AuthenticationResponse.Type.TOKEN, redirectUri);
 
         builder.setScopes(new String[]{"user-follow-read"});
         AuthenticationRequest request = builder.build();
