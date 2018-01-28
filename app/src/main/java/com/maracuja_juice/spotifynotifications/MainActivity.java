80014ba72fa994b02c0c1dbf26dab6fea159b874
MarimbaMaurice <mpanchaud@yahoo.de>
2018-01-28 14:21:01 +0100

    project setup

---
+++ app/src/main/java/com/maracuja_juice/spotifynotifications/MainActivity.java
@@ -0,0 +1,13 @@
+package com.maracuja_juice.spotifynotifications;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
