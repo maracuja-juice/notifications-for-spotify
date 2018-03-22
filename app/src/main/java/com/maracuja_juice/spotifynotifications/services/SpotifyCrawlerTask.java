9a23c231905d059c8a7667095eeb595d3f22c333
Marimba Maurice <mpanchaud@yahoo.de>
2018-03-22 14:35:05 +0100

    time from 6 seconds to 2 seconds if api>=24 lower: time from 6 seconds to 4 seconds #13

--- app/src/main/java/com/maracuja_juice/spotifynotifications/services/SpotifyCrawlerTask.java
+++ app/src/main/java/com/maracuja_juice/spotifynotifications/services/SpotifyCrawlerTask.java
@@ -1,35 +1,28 @@
 package com.maracuja_juice.spotifynotifications.services;
 
+import android.annotation.SuppressLint;
 import android.os.AsyncTask;
-import android.util.Log;
+import android.os.Build;
 
+import com.annimon.stream.Stream;
 import com.maracuja_juice.spotifynotifications.di.DaggerSpotifyApiComponent;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.inject.Inject;
 
 import kaaes.spotify.webapi.android.SpotifyApi;
-import kaaes.spotify.webapi.android.SpotifyCallback;
-import kaaes.spotify.webapi.android.SpotifyError;
 import kaaes.spotify.webapi.android.SpotifyService;
 import kaaes.spotify.webapi.android.models.Album;
 import kaaes.spotify.webapi.android.models.Artist;
-import kaaes.spotify.webapi.android.models.ArtistsCursorPager;
-import kaaes.spotify.webapi.android.models.Pager;
-import retrofit.Callback;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
 
 public class SpotifyCrawlerTask extends AsyncTask<Void, Void, List<Album>> {
 
     private static final String LOG_TAG = SpotifyCrawlerTask.class.getName();
-
+    @Inject
+    SpotifyApi api;
     private SpotifyService spotify;
-    @Inject SpotifyApi api;
     private OnTaskCompleted listener;
 
     private ArtistCrawler artistCrawler;
@@ -50,11 +43,20 @@ public class SpotifyCrawlerTask extends AsyncTask<Void, Void, List<Album>> {
         return artistCrawler.getFollowedArtists();
     }
 
+    @SuppressLint("NewApi")
     public List<Album> getAlbumsOfAllArtists(List<Artist> artists) {
         ArrayList<Album> albums = new ArrayList<>();
-        for (int i = 0; i < artists.size(); i++) {
-            List<Album> artistsAlbums = getAlbumsOfSingleArtist(artists.get(i));
-            albums.addAll(artistsAlbums);
+
+        boolean canUseParallelStream = Build.VERSION.SDK_INT >= Build.VERSION_CODES.N;
+        if (canUseParallelStream) {
+            artists.parallelStream().forEach(artist -> {
+                albums.addAll(getAlbumsOfSingleArtist(artist));
+            });
+        } else {
+            // backwards compatible streams api via library
+            Stream.of(artists).forEach((artist) -> {
+                albums.addAll(getAlbumsOfSingleArtist(artist));
+            });
         }
         return albums;
     }
