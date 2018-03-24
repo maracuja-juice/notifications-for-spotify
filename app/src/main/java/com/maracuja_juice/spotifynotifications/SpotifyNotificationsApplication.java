9b68d82d4277834feeba8c734f845d3cba264017
Marimba Maurice <mpanchaud@yahoo.de>
2018-03-24 14:52:00 +0100

    leak canary for memory leak detection

--- app/src/main/java/com/maracuja_juice/spotifynotifications/SpotifyNotificationsApplication.java
+++ app/src/main/java/com/maracuja_juice/spotifynotifications/SpotifyNotificationsApplication.java
@@ -2,6 +2,8 @@ package com.maracuja_juice.spotifynotifications;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
+
 import net.danlew.android.joda.JodaTimeAndroid;
 
 /**
@@ -12,6 +14,12 @@ public class SpotifyNotificationsApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
         JodaTimeAndroid.init(this);
     }
 }
