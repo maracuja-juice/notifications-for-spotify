73be068793351567a60540067bf3c1843449ec82
maracuja-juice <mpanchaud@yahoo.de>
2018-04-10 19:34:57 +0200

    add json property annotation for correct deserialization

--- app/src/main/java/com/maracuja_juice/spotifynotifications/api/model/AccessTokenResponse.java
+++ app/src/main/java/com/maracuja_juice/spotifynotifications/api/model/AccessTokenResponse.java
@@ -1,10 +1,17 @@
 package com.maracuja_juice.spotifynotifications.api.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class AccessTokenResponse {
+    @JsonProperty("access_token")
     private String accessToken;
+    @JsonProperty("token_type")
     private String tokenType;
+    @JsonProperty("expires_in")
     private int expiresIn;
+    @JsonProperty("refresh_token")
     private String refreshToken;
+    @JsonProperty("scope")
     private String scope;
 
     public AccessTokenResponse(String accessToken, String tokenType, int expiresIn, String refreshToken, String scope) {
--- app/src/main/java/com/maracuja_juice/spotifynotifications/api/model/RefreshTokenResponse.java
+++ app/src/main/java/com/maracuja_juice/spotifynotifications/api/model/RefreshTokenResponse.java
@@ -1,9 +1,15 @@
 package com.maracuja_juice.spotifynotifications.api.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class RefreshTokenResponse {
+    @JsonProperty("access_token")
     private String accessToken;
+    @JsonProperty("token_type")
     private String tokenType;
+    @JsonProperty("expires_in")
     private int expiresIn;
+    @JsonProperty("scope")
     private String scope;
 
     public RefreshTokenResponse(String accessToken, String tokenType, int expiresIn, String scope) {
